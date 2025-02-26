7353abba06353f00e09f2d82fa39bc9a6b25b0cc
Prad N <prad@didao.xyz>
2025-02-26 09:22:38 -0500

    feat: enable icon customization for docs and blog

--- lib/source.ts
+++ lib/source.ts
@@ -1,14 +1,33 @@
 import { docs, blog as blogPosts } from '@/.source';
 import { loader } from 'fumadocs-core/source';
 import { createMDXSource } from 'fumadocs-mdx';
+import { icons } from 'lucide-react';
+import { createElement } from 'react';
 
 export const source = loader({
   baseUrl: '/docs',
   source: docs.toFumadocsSource(),
+  icon(icon) {
+    if (!icon) {
+      // You may set a default icon
+      return;
+    }
+
+    if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
+  },
 });
 
 export const blog = loader({
   baseUrl: '/blog',
   source: createMDXSource(blogPosts),
+  icon(icon) {
+    if (!icon) {
+      // You may set a default icon
+      return;
+    }
+
+    if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
+  },
+
 });
 
